664be5f84cd4596d10e23a5ecc56018059dbc51f
OReznyk <oreznyk@outlook.com>
2020-10-30 18:50:21 +0300

    NEW: Users Fragment (ok)
    ToDo: email verification dependencies(do not continue to main without verification? ), comments; continue app

---
+++ app/src/main/java/com/splitter/Fragments/UsersFragment.java
@@ -0,0 +1,114 @@
+package com.splitter.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.splitter.Adapters.UsersAdapter;
+import com.splitter.Model.User;
+import com.splitter.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UsersFragment extends Fragment {
+    RecyclerView recyclerView;
+    UsersAdapter adapter;
+    List<User> userList;
+
+
+
+    public UsersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+        recyclerView = view.findViewById(R.id.users_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        
+        //init & all get users to list
+        //ToDo change all to friends only
+        userList = new ArrayList<>();
+        getAllUsers();
+        
+        return view;
+    }
+
+    private void getAllUsers() {
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+
+                    if(!fUser.getUid().equals(user.getId())){
+                        userList.add(user);
+                    }
+                    adapter = new UsersAdapter(getActivity(), userList);
+                    recyclerView.setAdapter(adapter);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void searchUsers(String s){
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!userList.isEmpty())userList.clear();
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+
+                    if(!user.getId().equals(fUser.getUid())){
+                        if(user.getName().toLowerCase().contains(s.toLowerCase()) || user.getEmail().toLowerCase().contains(s.toLowerCase())) {
+                            userList.add(user);
+                        }
+                    }
+                    adapter = new UsersAdapter(getActivity(), userList);
+                    adapter.notifyDataSetChanged();
+                    recyclerView.setAdapter(adapter);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //TODO fill onCanceled
+            }
+        });
+    }
+}
